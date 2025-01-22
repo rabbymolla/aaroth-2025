f3e8919de33826cc3942e4acf6c23083207649c4
M. SHOZAUL BARY <rabby16139@gmail.com>
2025-01-23 05:57:48 +0800

    flash

--- app/flashSale/page.js
+++ app/flashSale/page.js
@@ -2,6 +2,7 @@ import React from "react";
 import flashSaleBg from "@/public/PNG/flashSaleBg.png";
 import Image from "next/image";
 import FlashCountrPart from "@/components/flashItems/flashCountrPart";
+import FlashSlider from "@/components/flashItems/flashSlider";
 
 const FlashSale = () => {
   return (
@@ -13,12 +14,18 @@ const FlashSale = () => {
           </div>
           <div className="absolute top-0 left-0 bg-opacity_bg w-full h-full lg:rounded-[30px] ">
             <div className="absolute top-1/2 left-0 -translate-y-1/2 w-full px-3 lg:px-5">
-              <div className="grid grid-cols-12 gap-x-4">
+              <div className="grid grid-cols-12 gap-x-4 items-center justify-between">
                 <div className="col-span-5">
-                  {/* to check go to the components file */}
+                  {/* to check go to the flshItems of components file */}
                   <FlashCountrPart />
                 </div>
-                <div className="col-span-7">1</div>
+                <div className="col-span-7">
+                  {/* to check go to the flshItems of components file */}
+                  <FlashSlider />
+                  <button className="w-full text-end font-Roboto font-bold text-white_color text-base ">
+                    view more
+                  </button>
+                </div>
               </div>
             </div>
           </div>
---
+++ components/flashItems/flashSlider.js
@@ -0,0 +1,146 @@
+"use client";
+import React, { useState } from "react";
+import Slider from "react-slick";
+import flash1 from "@/public/PNG/flash1.png";
+import flash2 from "@/public/PNG/flash2.png";
+import Image from "next/image";
+import Link from "next/link";
+
+const sliderImg = [
+  {
+    id: 1,
+    img: flash1,
+    title: "Fitness and activity tracker",
+    price: "$400",
+  },
+  {
+    id: 2,
+    img: flash2,
+    title: "Fitness and activity tracker",
+    price: "$200",
+  },
+  {
+    id: 3,
+    img: flash1,
+    title: "Fitness and activity tracker",
+    price: "$300",
+  },
+  {
+    id: 4,
+    img: flash2,
+    title: "Fitness and activity tracker",
+    price: "$500",
+  },
+];
+
+const FlashSlider = () => {
+  const [active, setActive] = useState("");
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: false,
+    // nextArrow: <SampleNextArrow />,
+    // prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024, // Large screens
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 768, // Tablets
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 480, // Mobile screens
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+        },
+      },
+    ],
+    beforeChange: (prev, next) => {
+      setActive(next);
+    },
+    appendDots: (dots) => (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          //justify-content: space-between;
+        }}
+      >
+        <ul
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {dots}
+        </ul>
+      </div>
+    ),
+    customPaging: (i) => (
+      <div
+        className={`${
+          i === active
+            ? "bg-white_color p-1 rounded-full"
+            : "w-2 h-2 rounded-full bg-border_color"
+        }`}
+      >
+        <span className="w-1 h-1 rounded-full bg-border_color"></span>
+      </div>
+    ),
+  };
+  return (
+    <div className="slider-container">
+      <Slider {...settings}>
+        {sliderImg.map((data) => (
+          <div key={data.id} className=" md:p-4">
+            {/* Outer container with padding for gaps */}
+            <Link
+              href="#"
+              className="flex items-center gap-x-2 bg-white_color rounded-lg shadow-md py-2 px-2 sm:py-4 sm:px-3"
+            >
+              {/* Inner content */}
+              <div>
+                <Image
+                  src={data.img}
+                  alt={data.alt || "Flash Image"}
+                  priority={false}
+                  placeholder="blur"
+                />
+              </div>
+              <div>
+                <h1 className="text-xs md:text-lg font-Roboto font-medium text-black_color mb-5">
+                  {data.title}
+                </h1>
+                <p className="text-gray_color font-Roboto font-bold text-base sm:text-lg">
+                  {data.price}
+                </p>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default FlashSlider;

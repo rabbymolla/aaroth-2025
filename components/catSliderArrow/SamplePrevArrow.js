3799df7e76b67063461ff71531227555a3c97b18
M. SHOZAUL BARY <rabby16139@gmail.com>
2025-01-24 03:56:54 +0800

    ss

--- components/catSliderArrow/SamplePrevArrow.js
+++ components/catSliderArrow/SamplePrevArrow.js
@@ -3,7 +3,7 @@ export default function SamplePrevArrow(props) {
   const { onClick } = props;
   return (
     <div
-      className="absolute -top-[115px] sm:-top-[108px] right-16  -translate-y-1/2 rounded-full z-10 p-2 cursor-pointer text-gray_color hover:text-white_color  duration-100 ease-linear border border-border_color hover:bg-button_color"
+      className="absolute -top-[108px] right-16  -translate-y-1/2 rounded-full z-10 p-1 sm:p-2 cursor-pointer text-gray_color hover:text-white_color  duration-100 ease-linear border border-border_color hover:bg-button_color"
       onClick={onClick}
     >
       <FaAngleLeft size={20} />

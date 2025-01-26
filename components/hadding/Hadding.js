264d82ed9c2fddf42e3576bd009df453efa9f044
M. SHOZAUL BARY <rabby16139@gmail.com>
2025-01-26 22:47:51 +0800

    ohhhgh

--- components/hadding/Hadding.js
+++ components/hadding/Hadding.js
@@ -1,8 +1,8 @@
 export default function Hadding({ title, className }) {
   return (
     <h1
-      className={`font-Roboto font-bold  text-black_color mb-5 lg:mb-10 ${
-        className ? `${className}` : "text-2xl"
+      className={`font-Roboto font-bold text-black_color mb-5 lg:mb-10 text-lg ${
+        className ? `${className}` : "md:text-2xl"
       }`}
     >
       {title}
